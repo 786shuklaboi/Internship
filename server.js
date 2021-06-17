066a574ed2e9610b369fad51ff0ff0dc3b90bf91
786shuklaboi <46483575+786shuklaboi@users.noreply.github.com>
2021-06-17 13:55:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+app.get('/sum', (req, res) => {
+    let num1 = parseInt(req.query.num1)
+    let num2 = parseInt(req.query.num2)
+
+    res.send(sum(num1, num2))
+});
+
+
+app.listen(8080, () => {
+    console.log('Port 8080')
+});
+
+
+function sum(num1, num2){
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+return('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+}
